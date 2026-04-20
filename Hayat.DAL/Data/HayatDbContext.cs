b3d92adc77b35715ba4f80ca8b1a7fe7e7d0924e
Mohamed El-Qarram <mohamed0ibrahim0000@gmail.com>
2026-04-20 16:50:35 +0200

    Add project files.

---
+++ Hayat.DAL/Data/HayatDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Hayat.DAL.Data
+{
+    public class HayatDbContext: DbContext
+    {
+        public HayatDbContext(DbContextOptions<HayatDbContext> options): base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        base.OnModelCreating(modelBuilder);
+
+        // Apply all IEntityTypeConfiguration<T> classes from this assembly
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+
+
+    }
